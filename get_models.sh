d23d1b19844e31ba78c5393aae146a59e6b19934
Kurt Manninen <3514241+kmanninen@users.noreply.github.co>
2025-08-28 09:39:58 -0700

    Updates per pull #9 comments. Also removed YOLO and VWW.

--- get_models.sh
+++ get_models.sh
@@ -2,10 +2,8 @@ cd examples
 mkdir -p models
 cd models
 
-wget https://data.brainchip.com/models/AkidaV2/yolo/yolo_akidanet_voc_i8_w8_a8.h5
 wget https://data.brainchip.com/models/AkidaV2/tenn_spatiotemporal/tenn_spatiotemporal_eye_buffer_i8_w8_a8.h5
 wget https://data.brainchip.com/models/AkidaV2/tenn_spatiotemporal/tenn_spatiotemporal_jester_buffer_i8_w8_a8.h5
-wget https://data.brainchip.com/models/AkidaV2/akidanet/akidanet_vww_i8_w4_a4.h5
 wget https://data.brainchip.com/models/AkidaV2/centernet/centernet_akidanet18_voc_384_i8_w8_a8.h5
 
 find . -maxdepth 1 -type f \( -name "*.h5" \) -exec cnn2snn convert -m  {} \;
@@ -17,7 +15,6 @@ cd ..
 cd datasets
 
 wget https://data.brainchip.com/dataset-mirror/voc/test_20_classes.tfrecord
-wget https://data.brainchip.com/dataset-mirror/coco/coco_anchors.pkl
 
 wget https://data.brainchip.com/dataset-mirror/jester/jester_subset.tar.gz
 tar -xzf ./jester_subset.tar.gz  jester_subset/jester-v1-labels.csv 
